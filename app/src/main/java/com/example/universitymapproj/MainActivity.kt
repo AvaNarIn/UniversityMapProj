4824305eacc52abd36969c7eb66ac288d428374d
feuvrt <260897031+feuvrt@users.noreply.github.com>
2026-03-29 22:29:57 +0700

    new map (without useless text)

--- app/src/main/java/com/example/universitymapproj/MainActivity.kt
+++ app/src/main/java/com/example/universitymapproj/MainActivity.kt
@@ -30,10 +30,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlin.math.abs
 
-private const val COLS = 108
-private const val ROWS = 122
-private const val IMG_W = 1944f
-private const val IMG_H = 2196f
+private const val COLS = 216
+private const val ROWS = 244
+private const val IMG_W = 1170f
+private const val IMG_H = 1414f
 
 // ХРАНИЛИЩЕ
 object MapConfig {
