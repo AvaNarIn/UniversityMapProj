634034ad843b10b62781a54d90278d98b1730ea7
KRomaN2501 <219839865+KRomaN2501@users.noreply.github.com>
2026-03-31 21:10:42 +0700

    Revert "new map (without useless text)"
    This reverts commit 38d43fca5e5bc4b6fdaa02f589465f4142dcbeec.

--- app/src/main/java/com/example/universitymapproj/MainActivity.kt
+++ app/src/main/java/com/example/universitymapproj/MainActivity.kt
@@ -31,8 +31,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlin.math.abs
 
-private const val COLS = 108
-private const val ROWS = 122
+private const val COLS = 216
+private const val ROWS = 244
 private const val IMG_W = 1170f
 private const val IMG_H = 1414f
 
